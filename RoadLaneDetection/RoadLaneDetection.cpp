ad78bcf8173f2f142a440353032c321706b3e8d1
DESKTOP-QQ11LUI\choijoohee <choijoohee@naver.com>
2021-06-14 23:12:45 +0900

    가우시안 블러딩, Canny 에지 검출, 차 진행방향으로 영역 제한 구현

--- RoadLaneDetection/RoadLaneDetection.cpp
+++ RoadLaneDetection/RoadLaneDetection.cpp
@@ -1,5 +1,5 @@
-﻿#include "opencv2/opencv.hpp"
-#include "opencv2/highgui/highgui.hpp"
+﻿#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,11 +11,35 @@ float img_size, img_center;
 float left_m, right_m;
 Point left_b, right_b;
 
+
 //Region - of - interest vertices, 관심 영역 범위 계산시 사용 
-//We want a trapezoid shape, with bottom edge at the bottom of the image
 float trap_bottom_width = 0.85;  // width of bottom edge of trapezoid, expressed as percentage of image width
 float trap_top_width = 0.07;     // ditto for top edge of trapezoid
 float trap_height = 0.4;         // height of the trapezoid expressed as percentage of image height
+
+
+Mat limit_region(Mat img_edges, Point* points) {
+	/*
+		마스크를 적용한다.
+		polygon으로 정의된 이미지 영역만 지정한다.
+		나머지 영역은 검은색으로 적용.
+	*/
+	Mat output;
+	Mat mask = Mat::zeros(img_edges.rows, img_edges.cols, CV_8UC1);
+
+	Scalar ignore_mask_color = Scalar(255, 255, 255);
+	const Point* pts[1] = { points };
+	int npt[] = { 4 };
+
+
+	//정점으로 정의된 다각형 내부의 색상을 채운다.
+	fillPoly(mask, pts, npt, 1, Scalar(255, 255, 255), LINE_8);
+
+
+	//결과를 얻기 위해 edges 이미지와 mask를 곱한다.
+	bitwise_and(img_edges, mask, output);
+	return output;
+}
 
 int main()
 {
@@ -43,18 +67,40 @@ int main()
 		return -1;
 	}
 
+
 	video.read(img_bgr);
 	int cnt = 0;
 
-	while (1) { 
+	while (1) {
 		//1. 원본 영상을 읽어온다.
 		if (!video.read(img_bgr))
 			break;
 
+		//2. GrayScale 영상으로 변환
+		cvtColor(img_bgr, img_gray, COLOR_BGR2GRAY);
+
+		//3. 노이즈 제거를 위해 Gaussian Blur를 사용
+		GaussianBlur(img_gray, img_gray, Size(3, 3), 0, 0);
+
+		//4. Canny Edge Detection으로 에지를 추출
+		Canny(img_gray, img_edges, 50, 150);
+
+		int width = img_gray.cols;
+		int height = img_gray.rows;
+
+		Point points[4]{
+			Point((width * (1 - trap_bottom_width)) / 2, height),
+			Point((width * (1 - trap_top_width)) / 2, height - height * trap_height),
+			Point(width - (width * (1 - trap_top_width)) / 2, height - height * trap_height),
+			Point(width - (width * (1 - trap_bottom_width)) / 2, height)
+		};
+
+
+		//4. 자동차의 진행방향 바닥에 존재하는 차선만을 검출하기 위한 관심 영역을 지정
+		img_edges = limit_region(img_edges, points);
 	}
 	return 0;
 }
-
 
 
 
